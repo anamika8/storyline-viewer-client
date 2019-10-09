6fe908dd5c660f8b8dc0f5beeef89c0c1e4cf5e7
anamika8 <anamikaeight@gmail.com>
2019-10-08 20:18:01 -0700

    added test file

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,9 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow, mount } from 'enzyme';
 import App from "./app";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  it('Renders without crashing', () => {
+    shallow(<App />);
 });
