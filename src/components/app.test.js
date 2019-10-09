3f013039de039b38a0c04b321a09e35db99ba070
anamika8 <anamikaeight@gmail.com>
2019-10-08 20:22:27 -0700

    fixed error for test file

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { shallow, mount } from 'enzyme';
+import { shallow, mount } from "enzyme";
 import App from "./app";
 
 it("renders without crashing", () => {
-  it('Renders without crashing', () => {
+  it("Renders without crashing", () => {
     shallow(<App />);
+  });
 });
